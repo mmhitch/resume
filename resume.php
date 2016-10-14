56ac5b6bc93ea291dd3f8f226694a938374a5534
Mark Hitchcock <mmhitch10@gmail.com>
2016-10-13 23:04:18 -0700

    adding resume

---
+++ resume.php
@@ -0,0 +1,107 @@
+<!doctype html>
+<html lang="en">
+    <head>
+         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+         <meta name="viewport" content="width=device-width,
+							maximum-scale=1.0,
+							minimum-scale=1.0,
+							initial-scale=1.0" />
+         <title>Mark Hitchcock | Resume</title> 
+         <link rel="stylesheet" type="text/css" href="css/styles.css" media="screen" />
+         <link rel="stylesheet" type="text/css" href="css/screen_layout_large.css" />
+         <link rel="stylesheet" type="text/css" media="only screen and (min-width:50px) and (max-width:500px)" href="css/screen_layout_small.css"  />
+         <link rel="stylesheet" type="text/css" media="only screen and (min-width:501px) and (max-width:800px)" href="css/screen_layout_medium.css"  />
+         <link rel="stylesheet" type="text/css" href="css/print.css" media="print" />
+              
+         <!-- [if lt IE 9]>
+                <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+            <![endif]-->
+         <script>
+            function printpage()
+              {
+              window.print()
+              }
+         </script>
+    </head>
+    <body class='noprint'>
+      <div id='page'>
+	<header>
+	    <input class="noPrint right" type="button" value="Print this page" onclick="printpage()">
+	    <h1 class='center'>Mark Hitchcock</h1>
+	    <p class='center'>3431 S 255th St, Kent WA 98032 <a href='mailto:mark_hitchcock_10@hotmail.com'>mark_hitchcock_10 at hotmail.com</a> Tel 425-449-9894<br /></p>
+	    <hr><hr class="noPrint">
+         </header>
+	<section>
+	    <h2>Professional Statement</h2>
+	    <p>I am a positive, hardworking individual with a strong work ethic, excellent problem solving and relationship skills.  I love to learn and am up for a challenge in all situations. I get along well with others, whilst also working efficiently on my own. I am seeking a career position where I can develop and excel while contributing my technical and professional skills.</p>
+	    	    
+	    <h2>Work Experience</h2>
+		 <p><b>Product Creation Specialist</b> <span class='right'>(2013 - Current)</span><br />
+	    USCutter Redmond WA<br />
+	    Managing product webpages for over 3500 products.  Creating and configuring product pages with sku and product relationships. HTML formatting as well as SEO best practices. Experience with Google Analytics, Google Merchant Center, eBay, Adwords, Webmaster Tools, Adobe Photoshop/ Illustrator & Google Web Designer.  Maintain PHP driven technical support site.</p>
+	    <h4>Key Accomplishments:</h4>
+	    <ul>
+	       <li>Contributed to 20% annual growth in online sales and website traffic.</li>
+			 <li>Consistently meet deadlines for new product launch.</li>
+			 <li>Major role in freight and multiple state sales tax configurations.</li>
+	    </ul>
+	    <p><b>Freelance Web Developer</b> <span class='right'>(2012 - Current)</span><br />
+	    We Web Build Kent WA<br />
+	    Gained Experience creating websites for small businesses and organizations using HTML/CSS, HTML5/CSS3, XHTML, WP, CMS, JavaScript, PHP, and MySQL.  </p>
+	    <h4>Key Accomplishments:</h4>
+	    <ul>
+	       <li>Worked as Project Manager and DBA on project for Highline C.C. CSCI Alumni.</li>
+	       <li>Experience designing database and working with DDL and DML.  </li>
+	       <li>Developed data driven website with PHP and MySQL skills.  </li>
+          <li>Experienced as Agile Project Manager and using versioning software. </li>
+	    </ul>
+	    <p><b>Sales Manager</b><span class='right'>(Jan 2003- Nov 2011)</span><br />
+	    Renton Motorcycles 3701 E Valley Rd Renton WA 98057<br />
+	    Supervised up to 17 salespeople, selling motorcycles, making schedules, assigning tasks and negotiating deals. Responsibilities included salary adjustments, hiring, performance bonuses and training.  Professionally handled performance management, and coached direct reports. Oversaw and approved all work to new and used vehicles maintaining compliance with DOT and limiting liability. Proficient in Excel, Word, Office, PowerPoint and ADP for monthly tracking and setting goals, inventory, time card management, wholesale accounts, coordinating trade shows, sales incentives, vehicle licensing and transactions. Excellent communication and customer service skills.</p> 
+	    <h4>Key Accomplishments:</h4>
+	    <ul>
+	       <li>Implemented quality control inspection process for used vehicles.</li>
+	       <li>Introduced formal nine step sales process which resulted in increased sales.</li>
+	       <li>Introduced and trained staff on CRM program.</li>
+	       <li>Created customized phone script which streamlined sales process.</li>
+	    </ul>
+	    <p><b>Sales</b><span class='right'>(2000 - 2002)</span><br />
+	    Bellevue Kawasaki<br />
+	    Selling major units.  Excellent customer relation and negotioation skills.  Deliver sales process from greeting customers to following up.</p>
+	    <div class="noPrint">
+	    <p><b>Parts &amp; Accessories Sales</b><span class='right'>(1999 - 2000)</span><br />
+	    Everett Powersports<br />
+	    Light Speed experience, Honda, Suzuki, Sea Doo</p>
+		 
+	   	<p><b>Assistant Parts Manager/ Warranty Administrator</b><span class='right'>(Jan 1996- Nov 1999)</span><br />
+			Bellevue Kawasaki<br /> 14004 NE 20th Bellevue WA 9004<br />
+		   Retail Counter Sales of parts and accessories, shipping and receiving, cashiering, organizing parts orders and parts to repair orders and inventory. Use of microfiche, labor rate books, on-line parts schematics for warranty submissions to manufacturers.</p>
+		 </div>
+	</section>
+	<footer>
+	    <h2>Education</h2>
+	    <ul>
+	       <li>Highline Community College AAS Web/ Database Development Program, GPA 3.94 (2013)</li>
+			 <li>Highline Community College Certificate in Client Side Scripting (2013)</li>
+			 <li>Highline Community College Certificate in Server Side Scripting (2013)</li>
+	       <li>Microsoft Digital Literacy Certificate (2012)</li>
+	       <li>Ethos Group, Management Training (2008, Irving TX)</li>
+	       <li>Sobel and Associates, Comprehensive Sales, Write-Up & Close, Product & Process Seminars (2007)</li>
+	       <li>Paul Cummings, Level 10 Leadership, Goal Setting Sales Training (March 2005, Wildwood GA)</li>
+	       <li>Bellevue Community College courses in Environmental Science and Business</li>
+	       <li>Newport High School Bellevue WA - High School Diploma (Sept 1990-June 1994)</li>
+	       <li>Bellevue High School Automotive Technology (Completed 1993)</li>
+	    </ul>
+		 <!--<h2>Volunteer Work</h2>
+		 <ul>
+		  <li>Student Volunteer for Highline's Acodemy Camp.  Teaching Highschool students to code.</li>
+		 </ul>-->
+	</footer>
+      </div>
+    </body>
+</html>
+
+
+
+
+
